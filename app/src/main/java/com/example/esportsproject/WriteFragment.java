05f381b6b908194b4ff5c12eeff410529c29d142
pazer1 <elven404@gmail.com>
2019-05-17 17:40:45 +0900

    Merge branch 'master' of https://github.com/pazer1/EsportsProject into out
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/esportsproject/Adapter/RecyclerAdapter.java
    #	app/src/main/java/com/example/esportsproject/DetailFragment.java
    #	app/src/main/java/com/example/esportsproject/MainActivity.java
    #	app/src/main/res/layout/detail_fragment.xml
    #	app/src/main/res/layout/listview_item.xml

--- app/src/main/java/com/example/esportsproject/WriteFragment.java
+++ app/src/main/java/com/example/esportsproject/WriteFragment.java
@@ -24,7 +24,12 @@ public class WriteFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.write_fragment,container,false);
-
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
         return view;
     }
 
